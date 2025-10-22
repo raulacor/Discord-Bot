28c59e555260e678646f0f1cf39ed71927b94d9c
raulacor <raul.acorsi@gmail.com>
2025-10-22 08:18:53 -0300

    Add: Remove roles and handle errors

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import logging
 from dotenv import load_dotenv
 import os
 
+
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
@@ -14,7 +15,12 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
+#How to call for the bot:
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+#roles:
+role_1 = "fiaper"
+secret_role = "Gamer"
 
 @bot.event
 async def on_ready():
@@ -32,6 +38,37 @@ async def on_message(message):
 
     await bot.process_commands(message) #DO NOT DELETE, this allows the bot to continue to proccess all of the other messages.
 
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hello {ctx.author.mention}!")
 
+@bot.command()
+async def assign(ctx):
+    role = discord.utils.get(ctx.guild.roles, name=role_1)
+    if role:
+        await ctx.author.add_roles(role)
+        await ctx.send(f"{ctx.author.mention} is now assigned to {role_1}")
+    else:
+        await ctx.send("Role doesn't exist")
+
+@bot.command()
+async def remove(ctx):
+    role = discord.utils.get(ctx.guild.roles, name=role_1)
+    if role:
+        await ctx.author.remove_roles(role)
+        await ctx.send(f"{ctx.author.mention} has had the '{role_1}' role removed")
+    else:
+        await ctx.send("Role doesn't exist")
+
+@bot.command()
+@commands.has_role(role_1)
+async def secret(ctx):
+    await ctx.send("Welcome to the club!")
+
+@secret.error
+async def secret_error(ctx, error):
+    if isinstance(error, commands.MissingRole):
+        await ctx.send("You do not have permission to do that!")
+        
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
