8192d3f7541846ca0abdd9f4aa689514dbdc9174
raulacor <raul.acorsi@gmail.com>
2025-10-21 22:37:22 -0300

    Add: Fist events

--- main.py
+++ main.py
@@ -15,3 +15,23 @@ intents.message_content = True
 intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot loaded and ready to go, {bot.user.name}")
+
+@bot.event
+async def on_member_join(member):
+    await member.send(f"Welcome to the server {member.name}")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+
+    await bot.process_commands(message) #DO NOT DELETE, this allows the bot to continue to proccess all of the other messages.
+
+
+
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
